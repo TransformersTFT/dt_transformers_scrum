194d33ed7eeeb1327672ce48a6444278d0299338
Sergiodmp <126425497+Sergiodmp@users.noreply.github.com>
2023-05-06 18:27:53 +0200

    Add files via upload

---
+++ experiment.py
@@ -0,0 +1,273 @@
+###############este es el agente
+########  en evaluation se usan reset y step (agente)
+#######€  en models está el modelo
+#######luego tenemos el training
+
+
+import gym       #interface to reinforcement learning tasks
+import numpy as np
+import torch      #machine learning library used for developing and training neural networks. Unlike TensorFlow, PyTorch uses dynamic computation, which allows greater flexibility in building complex architectures. 
+import wandb      #to view the metrics of how the training is improving the model. Navigate in  https://wandb.ai/home to see it
+
+import argparse
+import pickle     #pickle as well as Json are forms of serialize data.It is a way to convert a data structure into a linear form that can be stored or transmitted over a network.
+import random
+import sys
+
+from evaluation_dt import evaluate_episode_rtg  
+from models.decision_transformer import DecisionTransformer
+from training import SequenceTrainer
+
+
+def discount_cumsum(x, gamma):
+    discount_cumsum = np.zeros_like(x)
+    discount_cumsum[-1] = x[-1]
+    for t in reversed(range(x.shape[0]-1)):
+        discount_cumsum[t] = x[t] + gamma * discount_cumsum[t+1]
+    return discount_cumsum
+
+
+def experiment(
+        exp_prefix,
+        variant,
+):
+    device = variant.get('device', 'cpu')
+    log_to_wandb = variant.get('log_to_wandb', False)
+    env_name =variant['env']
+    dataset = variant['dataset']            #we are designing the datasets as in D4RL benchmark to train the model
+    model_type = variant['model_type']
+    group_name = f'{exp_prefix}-{env_name}-{dataset}'
+    exp_prefix = f'{group_name}-{random.randint(int(1e5), int(1e6) - 1)}'
+
+    if env_name == 'env_va': 
+        from prueba_env import env_va
+        env = env_va()      #instanciamos la clase
+        max_ep_len = 1000
+        env_targets = [76, 40] # evaluation conditioning targets
+        scale = 1. # normalization for rewards/returns
+    else:
+        raise NotImplementedError
+    state_dim = env.observation_space.shape[0]   #it takes the shape that we have established in the environment
+    act_dim = env.action_space.n
+        # load dataset
+    dataset_path = f'data/{env_name}-{dataset}-v2.pkl'
+    with open(dataset_path, 'rb') as f:
+        trajectories = pickle.load(f)
+        #pickle deserializes the dataset as json does
+        # save all path information into separate lists
+    mode = variant.get('mode', 'normal')
+    states, traj_lens, returns = [], [], []
+    for path in trajectories:
+        if mode == 'delayed':  # delayed: all rewards moved to end of trajectory
+            path['rewards'][-1] = path['rewards'].sum()
+            path['rewards'][:-1] = 0.
+        states.append(path['observations'])
+        traj_lens.append(len(path['observations']))
+        returns.append(path['rewards'].sum())
+    traj_lens, returns = np.array(traj_lens), np.array(returns)
+
+    # used for input normalization
+    states = np.concatenate(states, axis=0)
+    state_mean, state_std = np.mean(states, axis=0), np.std(states, axis=0) + 1e-6
+
+    num_timesteps = sum(traj_lens)
+
+    print('=' * 50)
+    print(f'Starting new experiment: {env_name} {dataset}')
+    print(f'{len(traj_lens)} trajectories, {num_timesteps} timesteps found')
+    print(f'Average return: {np.mean(returns):.2f}, std: {np.std(returns):.2f}')
+    print(f'Max return: {np.max(returns):.2f}, min: {np.min(returns):.2f}')
+    print('=' * 50)
+
+    K = variant['K']
+    batch_size = variant['batch_size']
+    num_eval_episodes = variant['num_eval_episodes']
+    pct_traj = variant.get('pct_traj', 1.)
+ # only train on top pct_traj trajectories (for %BC experiment)
+    num_timesteps = max(int(pct_traj*num_timesteps), 1)
+    sorted_inds = np.argsort(returns)  # lowest to highest
+    num_trajectories = 1
+    timesteps = traj_lens[sorted_inds[-1]]
+    ind = len(trajectories) - 2
+    while ind >= 0 and timesteps + traj_lens[sorted_inds[ind]] <= num_timesteps:
+        timesteps += traj_lens[sorted_inds[ind]]
+        num_trajectories += 1
+        ind -= 1
+    sorted_inds = sorted_inds[-num_trajectories:]
+
+    # used to reweight sampling so we sample according to timesteps instead of trajectories
+    p_sample = traj_lens[sorted_inds] / sum(traj_lens[sorted_inds])
+
+
+    def get_batch(batch_size=256, max_len=K):
+        batch_inds = np.random.choice(
+            np.arange(num_trajectories),
+            size=batch_size,
+            replace=True,
+            p=p_sample,  # reweights so we sample according to timesteps
+        )
+
+        s, a, r, d, rtg, timesteps, mask = [], [], [], [], [], [], []
+        for i in range(batch_size):
+            traj = trajectories[int(sorted_inds[batch_inds[i]])]
+            si = random.randint(0, traj['rewards'].shape[0] - 1)
+
+            # get sequences from dataset
+            s.append(traj['observations'][si:si + max_len].reshape(1, -1, state_dim))
+            a.append(traj['actions'][si:si + max_len].reshape(1, -1, act_dim))
+            r.append(traj['rewards'][si:si + max_len].reshape(1, -1, 1))
+            if 'terminals' in traj:
+                d.append(traj['terminals'][si:si + max_len].reshape(1, -1))
+            else:
+                d.append(traj['dones'][si:si + max_len].reshape(1, -1))
+            timesteps.append(np.arange(si, si + s[-1].shape[1]).reshape(1, -1))
+            timesteps[-1][timesteps[-1] >= max_ep_len] = max_ep_len-1  # padding cutoff
+            rtg.append(discount_cumsum(traj['rewards'][si:], gamma=1.)[:s[-1].shape[1] + 1].reshape(1, -1, 1))
+            if rtg[-1].shape[1] <= s[-1].shape[1]:
+                rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1, 1))], axis=1)
+
+            # padding and state + reward normalization
+            tlen = s[-1].shape[1]
+            s[-1] = np.concatenate([np.zeros((1, max_len - tlen, state_dim)), s[-1]], axis=1)
+            s[-1] = (s[-1] - state_mean) / state_std
+            a[-1] = np.concatenate([np.ones((1, max_len - tlen, act_dim)) * -10., a[-1]], axis=1)
+            r[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), r[-1]], axis=1)
+            d[-1] = np.concatenate([np.ones((1, max_len - tlen)) * 2, d[-1]], axis=1)
+            rtg[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), rtg[-1]], axis=1) / scale
+            timesteps[-1] = np.concatenate([np.zeros((1, max_len - tlen)), timesteps[-1]], axis=1)
+            mask.append(np.concatenate([np.zeros((1, max_len - tlen)), np.ones((1, tlen))], axis=1))
+
+        s = torch.from_numpy(np.concatenate(s, axis=0)).to(dtype=torch.float32, device=device)
+        a = torch.from_numpy(np.concatenate(a, axis=0)).to(dtype=torch.float32, device=device)
+        r = torch.from_numpy(np.concatenate(r, axis=0)).to(dtype=torch.float32, device=device)
+        d = torch.from_numpy(np.concatenate(d, axis=0)).to(dtype=torch.long, device=device)
+        rtg = torch.from_numpy(np.concatenate(rtg, axis=0)).to(dtype=torch.float32, device=device)
+        timesteps = torch.from_numpy(np.concatenate(timesteps, axis=0)).to(dtype=torch.long, device=device)
+        mask = torch.from_numpy(np.concatenate(mask, axis=0)).to(device=device)
+
+        return s, a, r, d, rtg, timesteps, mask
+
+    def eval_episodes(target_rew):
+        def fn(model):
+            returns, lengths = [], []
+            for _ in range(num_eval_episodes):
+                with torch.no_grad():
+                    if model_type == 'dt':
+                        ret, length = evaluate_episode_rtg(
+                            env,
+                            state_dim,
+                            act_dim,
+                            model,
+                            max_ep_len=max_ep_len,
+                            scale=scale,
+                            target_return=target_rew/scale,
+                            mode=mode,
+                            state_mean=state_mean,
+                            state_std=state_std,
+                            device=device,
+                        )
+                        returns.append(ret)
+                lengths.append(length)
+            return {
+                f'target_{target_rew}_return_mean': np.mean(returns),
+                f'target_{target_rew}_return_std': np.std(returns),
+                f'target_{target_rew}_length_mean': np.mean(lengths),
+                f'target_{target_rew}_length_std': np.std(lengths),
+            }
+        return fn
+
+    if model_type == 'dt':
+        model = DecisionTransformer(
+            state_dim=state_dim,
+            act_dim=act_dim,
+            max_length=K,
+            max_ep_len=max_ep_len,
+            hidden_size=variant['embed_dim'],
+            n_layer=variant['n_layer'],
+            n_head=variant['n_head'],
+            n_inner=4*variant['embed_dim'],
+            activation_function=variant['activation_function'],
+            n_positions=1024,
+            resid_pdrop=variant['dropout'],
+            attn_pdrop=variant['dropout'],
+        )
+    else:
+        raise NotImplementedError
+
+    model = model.to(device=device)
+
+    warmup_steps = variant['warmup_steps']
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=variant['learning_rate'],
+        weight_decay=variant['weight_decay'],
+    )
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer,
+        lambda steps: min((steps+1)/warmup_steps, 1)
+    )
+
+    if model_type == 'dt':
+        trainer = SequenceTrainer(
+            model=model,
+            optimizer=optimizer,
+            batch_size=batch_size,
+            get_batch=get_batch,
+            scheduler=scheduler,
+            loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2),
+            eval_fns=[eval_episodes(tar) for tar in env_targets],
+        )
+
+    for iter in range(variant['max_iters']):
+        outputs = trainer.train_iteration(num_steps=variant['num_steps_per_iter'], iter_num=iter+1, print_logs=True)
+        if log_to_wandb:
+            wandb.log(outputs)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='env_va')
+    parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
+    parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
+    parser.add_argument('--K', type=int, default=20)
+    parser.add_argument('--pct_traj', type=float, default=1.)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer
+    parser.add_argument('--embed_dim', type=int, default=128)
+    parser.add_argument('--n_layer', type=int, default=3)
+    parser.add_argument('--n_head', type=int, default=1)
+    parser.add_argument('--activation_function', type=str, default='relu')
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
+    parser.add_argument('--warmup_steps', type=int, default=10000)
+    parser.add_argument('--num_eval_episodes', type=int, default=100)
+    parser.add_argument('--max_iters', type=int, default=10)
+    parser.add_argument('--num_steps_per_iter', type=int, default=10000)
+    parser.add_argument('--device', type=str, default='cpu')     # I do not have NVIDIA GNU so I can not use the toolkit
+    parser.add_argument('--log_to_wandb', '-w', type=bool, default=False)
+    
+    '''
+    #######################ARGUMENTS VA
+    parser.add_argument('-an', '--agent_number', type=int, metavar='', required=False, default=8, help='agent_number: 8, 9, 10, 11, 12')
+    parser.add_argument('-s', '--status', type=str, metavar='', required=False, default='pre-auction', help='status_var: pre-auction, auction, stand-by, Off')
+    parser.add_argument('-st', '--stop_time', type=int, metavar='', required=False, default=84600, help='stop_time: time in seconds where agent')
+    parser.add_argument('-sab', '--start_auction_before', type=int, metavar='', required=False, default=10, help='start_auction_before: seconds to start auction prior to current fab ends')
+    parser.add_argument('-sd', '--speed', type=float, metavar='', required=False, default=0.25, help='VA speed. Example --speed 0.25 ')
+    parser.add_argument('-w', '--wait_msg_time', type=int, metavar='', required=False, default=15, help='wait_msg_time: time in seconds to wait for a msg')
+    #
+    # MANAGEMENT DATA
+    parser.add_argument('-u', '--user_name', type=str, metavar='', required=False, help='User to the XMPP platform')  # JOM 10/10
+    parser.add_argument('-p', '--user_passwd', type=str, metavar='', required=False, help='Passwd for the XMPP platform')  # JOM 10/10
+    parser.add_argument('-lag', '--log_agnt_id', type=str, metavar='', required=False, help='User ID for the log agent')
+
+    #parameters coil orders
+    parser.add_argument('-bag', '--brw_agnt_id', type=str, metavar='', required=False, help='User ID for the browser agent')    
+    parser.add_argument('-os', '--oel_sorte', type=int, metavar='', required=False, default=5, help='oel_sorte: 0-10')   #Sergio 28/03
+    parser.add_argument('-sr', '--single_reduction', type=int, metavar='', required=False, default=1, help='single_reduction: 0 single red, 1 double red')   #Sergio 28/03
+
+
+'''
+    args = parser.parse_args()
+
+    experiment('gym-experiment', variant=vars(args))
---
+++ prueba_env.py
@@ -0,0 +1,245 @@
+import datetime
+import random
+import torch
+
+import pandas as pd
+import gym
+import numpy as np
+import plotly.figure_factory as ff
+import openpyxl
+import operative_functions as asf
+import globals
+
+global results_2, winner_df, all_winner_df, snapshot
+
+class env_va(gym.Env):
+        def __init__(self):
+                self.price_energy_consumption = 0.222 #euros/KWh
+                self.index_va = ['va09', 'va10', 'va11', 'va11', 'va12', 'va12']
+                self.df_parameters_energy=pd.DataFrame({
+                                'a': [-4335, -4335, -8081.22, -141,-6011.6, -3855.45],
+                                'b': [2.1, 2.1, 4.31, 3.27, 3.83, 2.4],
+                                'c': [5405.53, 5405.53, 6826.2, 5943.73, 6742.25, 901.87],
+                                'd': [191.27, 191.27, 240.12, 228.9, 195.85, 292.9],
+                                'e': [212.31, 212.31, 319.5, 348.29, 264.99, 238.68],
+                                'f': [9.44, 9.44, 12.68, 12.16, 11.74, 8.66]}, 
+                                index=self.index_va)
+
+                ############################################################################ instance variables of the class 
+                ###############################basic variables
+
+                self.snapshot = pd.read_excel(io='C:/Users/sergy/OneDrive/Documentos/MASTER/TFM/pruebas_sin_servidor/production_snapshot_pruebas.xlsx',
+                                                        sheet_name='Hoja1', header=0, names = None, index_col = None,
+                                                        usecols= 'A:T', engine= 'openpyxl')
+
+                #print(self.snapshot)
+                self.jobs=len(self.snapshot.axes[0]) #self jobs must reduce until 0
+                #print(self.jobs)
+
+                '''We define the max time steps in the experiment file''' 
+                #self.max_time_steps=1000 #maximum number of time steps in an episode. It is important because
+                        #it limits the duration between the agent and the environment. Without this paprameter, the interaction
+                        #could go on indefinitely, which would not be ideal for training the agent
+                self.current_time_step=0
+                self.action_space = gym.spaces.Discrete(self.jobs) #you can move from processing one coil to any other
+                #print(self.action_space.n)
+                self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(self.jobs,)) #the observation space is the current cost each time.
+                        #This includes if each coil meets the plant rules, the difference of thickness...
+                #####################################extra variables     
+                        
+                self.winner_df = pd.DataFrame()     
+                self.all_winner_df =   pd.DataFrame()  
+                self.assess_costs_coil = asf.va_bid_evaluation(self.df_parameters_energy,self.snapshot,self.price_energy_consumption,self.winner_df)
+                self.jid_list_2=self.assess_costs_coil.loc[:,'Coil number example'].tolist()
+                self.results_2 = asf.va_result(self.assess_costs_coil,self.jid_list_2)
+                #print(results_2)
+                #costcoils=np.random.rand(jobs)  #cost of the products randomly calculated and scalated between 0 and 1
+                self.costcoils_df=self.results_2.loc[:,'cost']
+                self.costcoils=self.costcoils_df.to_numpy()
+                #print(costcoils)
+                self.max_cost=self.results_2.iloc[-1]['cost']
+                self.coil_with_min_cost=self.results_2.iloc[0]['Coil']
+
+
+                self.plants_df=self.snapshot["PLANT"].value_counts()                    #dataframe that says the number of coils that each plant has to process
+                self.plants_number=len(self.plants_df.axes[0])
+                self.orders_df=self.snapshot["Production_Order_NR"].value_counts()      #dataframe that says the number of different orders of coils
+                self.orders_number=len(self.orders_df.axes[0])
+                act_dim=self.jobs
+                #actions = torch.zeros((1, act_dim), device='cpu', dtype=torch.float32)
+                #print(actions)
+                #state = #reset_model()
+                ###################################################actions are decided in the evaluate_episodes file
+                '''                for t in range(self.max_time_steps):
+                        
+
+                        #############actions are made with torch
+                        self.actions = torch.zeros((1, act_dim), device='cpu', dtype=torch.float32)
+                        #actions = torch.tensor([[1, 2], [3, 4], [5, 6]], dtype=torch.float32)
+                        self.action= torch.zeros_like(self.actions[-1])
+                        self.actions[-1] = action
+                        action = action.detach().cpu().numpy()
+                        ##############tensors are a type of data, similar to numpy arrays but in this case, they can be run on gpu to accelerate the training.
+                        self.state, self.reward, self.done, _ = self.step(self.action)
+                        
+                        
+                        #print(winner_df)
+                        '''
+
+                '''
+                observation_space = gym.spaces.Dict(
+                        {
+                        "action_mask": gym.spaces.Box(0, 1, shape=jobs + 1,)),
+                        "real_obs": gym.spaces.Box(
+                                low=0.0, high=1.0, shape=jobs, 22), dtype=float
+                        ),
+                        }
+                )
+                '''
+                '''
+                def _get_current_state_representation(self):
+                state[:, 0] =legal_actions[:-1]
+                        return {
+                        "real_obs":state,
+                        "action_mask":legal_actions,
+                }
+
+                def get_legal_actions(self):
+                        returnlegal_actions
+                '''
+
+        
+        def reward_scaler(self,reward):
+                self.max_cost=self.results_2.iloc[-1]['cost']
+                print("El max cost es:")
+                print(reward)
+                print(self.max_cost)
+                return reward /self.max_cost
+
+        def calculate_reward(self,action):
+                reward=0
+                reward= -1*self.costcoils[action]
+                scaled_reward=self.reward_scaler(reward)
+
+                return scaled_reward
+                
+        def reset(self):
+                self.current_time_step=0
+                self.snapshot = pd.read_excel(io='C:/Users/sergy/OneDrive/Documentos/MASTER/TFM/pruebas_sin_servidor/production_snapshot_pruebas.xlsx',
+                                                        sheet_name='Hoja1', header=0, names = None, index_col = None,
+                                                        usecols= 'A:T', engine= 'openpyxl')
+
+                self.winner_df = pd.DataFrame()     
+                self.all_winner_df =   pd.DataFrame()  
+                self.assess_costs_coil = asf.va_bid_evaluation(self.df_parameters_energy,self.snapshot,self.price_energy_consumption,self.winner_df)
+                self.jid_list_2=self.assess_costs_coil.loc[:,'Coil number example'].tolist()
+                self.results_2 = asf.va_result(self.assess_costs_coil,self.jid_list_2)
+                #print(results_2)
+                #costcoils=np.random.rand(jobs)  #cost of the products randomly calculated and scalated between 0 and 1
+                self.costcoils_df=self.results_2.loc[:,'cost']
+                self.costcoils=self.costcoils_df.to_numpy()
+                #print(costcoils)
+                self.max_cost=self.results_2.iloc[-1]['cost']
+                self.coil_with_min_cost=self.results_2.iloc[0]['Coil']
+                return self.costcoils
+
+
+
+        def step(self,action):
+                self.current_time_step+=1
+                #if action < len(self.costcoils_df):
+                if action < (len(self.results_2))-1:
+                        #print(action)
+                        print("El tamaño de self.results_2 es :", self.results_2.shape)
+                        #1. evaluo
+                        #2.ordeno por costes.
+                        #3.cogemos el que deberia haber ganado
+                        #4 calculamos el coste min para eliminarlo del snapshot.
+                        #obtenemos el coste de la accion aleatoria.
+
+                        self.assess_costs_coil = asf.va_bid_evaluation(self.df_parameters_energy,self.snapshot,self.price_energy_consumption,self.winner_df) 
+                        self.jid_list_2=self.assess_costs_coil.loc[:,'Coil number example'].tolist()
+                        self.results_2 = asf.va_result(self.assess_costs_coil,self.jid_list_2)
+                        #####self.winner_df =self.results_2.iloc[[0]] 
+                        
+                                                 #esto es por costes
+                        print("Tamaño del df de costcoils:",self.costcoils_df.shape)
+
+                        print("La accion es :",action)
+
+                        self.winner_df = self.results_2.iloc[[action]]                          #el ganador es obviamente el que se elige.
+                        self.winner_df = self.winner_df.reset_index(drop=True)                  
+                        self.coil_with_min_cost=self.results_2.iloc[0]['Coil'] 
+                        self.costcoils_df=self.results_2.loc[:,'cost'] 
+                        self.costcoils=self.costcoils_df.to_numpy()            
+                        self.costcoils[action] = self.costcoils_df.to_numpy()[action]
+                        #print("Tamaño del df de costcoils:",self.costcoils_df.shape)
+                        #print("tamaño del vector:", self.costcoils.shape)
+                        self.all_winner_df = pd.concat([self.all_winner_df,self.winner_df])              
+                        self.all_winner_df =self.all_winner_df.reset_index(drop=True)
+                        #print(self.costcoils_df)
+
+                        #self.costcoils[action]=self.costcoils_df.to_numpy()[action]                    #cost of the action
+                        #update the snapshot and winner_dataframe. We drop the coil with min cost and we added to the winner dataframe
+                        # reset the index of winner_df
+                        #self.snapshot.drop(self.snapshot[(self.snapshot['Coil number example']==self.coil_with_min_cost)].index, inplace =True)
+                        self.snapshot.drop(self.snapshot[(self.snapshot['Coil number example']==action)].index, inplace =True)
+                        self.snapshot = self.snapshot.reset_index(drop=True)
+                        #print(self.winner_df)
+                        #print("--------------------------------------------------------")
+
+                        print(self.all_winner_df)
+                        #update current time step
+                        #calculate reward
+                        reward=self.calculate_reward(action)
+                        print("-------------------------This is the reward---------------")
+                        print(reward)
+                        #update state
+                
+                else:
+                        reward=0    
+                        self.assess_costs_coil = asf.va_bid_evaluation(self.df_parameters_energy,self.snapshot,self.price_energy_consumption,self.winner_df) 
+                        self.jid_list_2=self.assess_costs_coil.loc[:,'Coil number example'].tolist()
+                        self.results_2 = asf.va_result(self.assess_costs_coil,self.jid_list_2)
+                        self.coil_with_min_cost=self.results_2.iloc[0]['Coil'] 
+                        self.costcoils_df=self.results_2.loc[:,'cost'] 
+                        #print(self.costcoils_df)            
+                        #self.costcoils[action] = self.costcoils_df.to_numpy()[action]
+
+                        '''Si la acción es más grande que el indice máximo de costcoils se sobreescribe el all_winner_df'''
+                        #update_state(action)
+                        #check if the episode is done
+                        #done=self.current_time_step>self.max_time_steps
+                state=self.costcoils
+                if self.snapshot.empty:
+                        done=True
+                else:
+                        done=False
+
+                #create info dictionary
+                info={}
+
+                #return observation, reward,done and info
+                return state, reward, done, info
+
+                '''
+                
+                reward = 0.0
+                if action ==jobs:
+                        whilenon_assigned_jobs != 0:
+                                reward -=increase_cost_step()
+                        scaled_reward =_reward_scaler(reward)
+                _check_no_op()
+                        return (
+                _get_current_state_representation(),
+                        scaled_reward,
+                _is_done(),
+                        {},)
+                '''
+
+
+
+
+
+
+
